550517f8c6b70954cfb3a38699e7bcd428b103f4
Kovlin <romainlink@hotmail.com>
2022-04-12 00:19:19 +0200

    su

--- setup.sh
+++ setup.sh
@@ -12,8 +12,8 @@ fi
 
 # on Linux
 
-mkdir -p /home/rlinkov/data/wp_volume
-mkdir -p /home/rlinkov/data/db_volume
+echo 'user42' | sudo -S mkdir -p /home/rlinkov/data/wp_volume
+echo 'user42' | sudo -S mkdir -p /home/rlinkov/data/db_volume
 
 echo 'user42' | sudo -S chmod 777 /etc/hosts
 sed -i 's/localhost/rlinkov.42.fr/g' /etc/hosts
