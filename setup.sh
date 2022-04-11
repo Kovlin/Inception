772ed3d6ccf4e8a479d1cf400291b4f2b8825963
Kovlin <romainlink@hotmail.com>
2022-04-12 00:49:14 +0200

    setup

--- setup.sh
+++ setup.sh
@@ -18,3 +18,6 @@ echo 'user42' | sudo -S mkdir -p /home/rlinkov/data/db_volume
 echo 'user42' | sudo -S chmod 777 /etc/hosts
 echo 'user42' | sudo -S sed -i 's/localhost/rlinkov.42.fr/g' /etc/hosts
 echo 'user42' | sudo -S chmod 0444 /etc/hosts
+
+echo 'user42' | sudo -S service nginx stop
+echo 'user42' | sudo -S systemctl start docker
