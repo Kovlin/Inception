39ce1b3945b6f6821dc7be95b7bca4fbf51343cd
Kovlin <romainlink@hotmail.com>
2022-04-12 01:27:20 +0200

    host

--- setup.sh
+++ setup.sh
@@ -16,7 +16,9 @@ echo 'user42' | sudo -S mkdir -p /home/rlinkov/data/wp_volume
 echo 'user42' | sudo -S mkdir -p /home/rlinkov/data/db_volume
 
 echo 'user42' | sudo -S chmod 777 /etc/hosts
-echo 'user42' | sudo -S sed -i 's/localhost/rlinkov.42.fr/g' /etc/hosts
+echo 'user42' | sudo -S hostnamectl set-hostname rlinkov.42.fr
+echo 'user42' | sudo -S sed -i 's/salty-VirtualBox/rlinkov.42.fr/g' /etc/hosts
+echo 'user42' | sudo -S sed -i '3i 127.0.0.1    rlinkov.42.fr/g' /etc/hosts
 echo 'user42' | sudo -S chmod 0444 /etc/hosts
 
 echo 'user42' | sudo -S service nginx stop
